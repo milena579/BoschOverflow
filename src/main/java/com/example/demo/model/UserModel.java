d2f6fc1ea93f555d6d4b92fd6ecba48d70222910
WallyDev <wallace07abreu@gmail.com>
2024-11-12 10:00:12 -0300

    Space Model Created

--- src/main/java/com/example/demo/model/UserModel.java
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -1,10 +1,10 @@
+package com.example.demo.model;
 
-import java.lang.annotation.Inherited;
-
-import javax.annotation.processing.Generated;
-
-import sun.jvm.hotspot.gc.shared.Generation;
-import sun.reflect.generics.reflectiveObjects.GenericArrayTypeImpl;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 
 @Entity
