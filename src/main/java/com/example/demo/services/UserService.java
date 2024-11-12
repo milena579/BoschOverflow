765c496b999e1898f78d77e7cdab87318ee209fa
milena579 <milecalegari@gmail.com>
2024-11-12 11:29:20 -0300

    service user

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -6,6 +6,9 @@ import com.example.demo.model.UserModel;
 
 public interface UserService {
     public List<UserModel> findAllUsers();
-    public String Login();
+    public UserModel Login(Long edv, String email);
     public String Register();
+    Boolean validaNome(String name);
+    Boolean validaEmail(String email);
+    Boolean validaSenha(String password);
 }
