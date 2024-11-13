d67234b3edcc8c6eac8e57d2f611e0afe9af61d1
Dudous <sampaioeeduardo36@gmail.com>
2024-11-13 11:05:07 -0300

    validations de usuário

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,11 +1,12 @@
-package com.example.demo.controllers
+package com.example.demo.controllers;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     
     @PostMapping
-    public ResponseEntity<String> login(@ResquestBody UserLoginData data) {
+    public ResponseEntity<String> create(@ResquestBody UserData data) {
+
         
     }
 }
