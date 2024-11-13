9a16939c357a56e54d4697f9a11eba65c267b06a
WallyDev <wallace07abreu@gmail.com>
2024-11-13 09:03:08 -0300

    Adicionado Querys de space e question

--- src/main/java/com/example/demo/services/QuestionService.java
+++ src/main/java/com/example/demo/services/QuestionService.java
@@ -1,9 +1,13 @@
 package com.example.demo.services;
 
+import java.util.List;
+
+import com.example.demo.dto.QuestionQuery;
 import com.example.demo.model.QuestionModel;
 
 public interface QuestionService {
     QuestionModel createQuestion();
     String deleteQuestion (Long questionId);
+    List<QuestionModel> searchQuestion(QuestionQuery query);
     QuestionModel editQuestion(Long questionId);
 }
--- src/main/java/com/example/demo/services/SpaceService.java
+++ src/main/java/com/example/demo/services/SpaceService.java
@@ -1,7 +1,16 @@
 package com.example.demo.services;
 
+import java.util.List;
+
+import com.example.demo.dto.SpaceQuery;
+
+import com.example.demo.model.SpaceModel;
+
+
+
 public interface SpaceService {
     String createSpace();
     String deleteSpace(Long spaceId);
+    List<SpaceModel> searchQuestion(SpaceQuery query);
     String givePermission(Long userId, Long spaceId);
 }
